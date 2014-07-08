c8f28148523a027dfcc9c384e6947c4b20aae836
Richard Walker <digitalsadhu@gmail.com>
2014-07-08 09:36:42 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function () {
+var request = require('request')
 
+module.exports = function (url) {
+  return function notify(message) {
+    request.get(url + '=' + message, function () {
+      console.log('[INFO]', 'message', message, 'sent to chatroom')
+    })
+  }
 }
